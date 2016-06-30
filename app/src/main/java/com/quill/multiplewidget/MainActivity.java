72fb6bd067b21a656c9032e34968f7fcf8d0d6ec
quillchen <quillchen@163.com>
2016-06-30 19:58:55 +0800

    add app files

---
+++ app/src/main/java/com/quill/multiplewidget/MainActivity.java
@@ -0,0 +1,50 @@
+package com.quill.multiplewidget;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.quill.multiplewidget.ui.SlideMenuActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private ListView mListView;
+    private List<String> activities;
+    private List<Class<?>> classes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mListView = (ListView) findViewById(R.id.main_list);
+        getData();
+        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, R.layout.item_main,activities);
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(this);
+    }
+
+
+    public void getData() {
+        classes = new ArrayList<>();
+        classes.add(SlideMenuActivity.class);//添加Activity
+
+        activities = new ArrayList<>();
+        for (Class<?> clazz: classes) {
+            activities.add(clazz+"");
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent=new Intent(MainActivity.this,classes.get(position));
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/quill/multiplewidget/ui/SlideMenuActivity.java
@@ -0,0 +1,16 @@
+package com.quill.multiplewidget.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.quill.multiplewidget.R;
+
+public class SlideMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slide_menu);
+
+    }
+}
